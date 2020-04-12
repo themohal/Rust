003ccb37a79ed3759fe2624bcee59e5205317c23
themohal <farjadmohal@gmail.com>
2020-04-12 08:49:34 +0500

    initial commit

---
+++ Quarter 2/ch10/generic_enum/target/debug/deps/generic_enum-431f292e0bde2116.d
@@ -0,0 +1,5 @@
+/home/farjad-ali/IoT/Quarter 2/ch10/generic_enum/target/debug/deps/generic_enum-431f292e0bde2116: src/main.rs
+
+/home/farjad-ali/IoT/Quarter 2/ch10/generic_enum/target/debug/deps/generic_enum-431f292e0bde2116.d: src/main.rs
+
+src/main.rs:
---
+++ Quarter 2/ch10/generic_enum/target/debug/generic_enum.d
@@ -0,0 +1,1 @@
+/home/farjad-ali/IoT/Quarter\ 2/ch10/generic_enum/target/debug/generic_enum: /home/farjad-ali/IoT/Quarter\ 2/ch10/generic_enum/src/main.rs
