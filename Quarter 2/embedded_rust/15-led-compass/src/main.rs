710bc67214964aed4ed345f271b480d3e4afb8f3
themohal <farjadmohal@gmail.com>
2020-09-09 10:05:40 -0700

    qibla direction with leds compass at home

--- Quarter 2/embedded_rust/15-led-compass/src/main.rs
+++ Quarter 2/embedded_rust/15-led-compass/src/main.rs
@@ -3,32 +3,46 @@
 #![no_std]
 
 #[allow(unused_imports)]
-use aux15::{entry, iprint, iprintln, prelude::*,Direction, I16x3};
+use core::f32::consts::PI;
+
+#[allow(unused_imports)]
+use aux15::{entry, iprint, iprintln, prelude::*, Direction, I16x3};
 use m::Float;
 
 #[entry]
 fn main() -> ! {
-    let (mut leds, mut lsm303dlhc, mut delay, _itm) = aux15::init();
+    let (mut leds, mut lsm303dlhc, mut delay, mut itm) = aux15::init();
 
     loop {
         let I16x3 { x, y, .. } = lsm303dlhc.mag().unwrap();
 
-        // Look at the signs of the X and Y components to determine in which
-        // quadrant the magnetic field is
-        let dir = match (x > 0, y > 0) {
-            // Quadrant I
-            (true, true) => Direction::Northeast,
-            // Quadrant II
-            (false, true) => Direction::Northwest,
-            // Quadrant III
-            (false, false) => Direction::Southwest,
-            // Quadrant IV
-            (true, false) => Direction::Southeast,
+        let theta = (y as f32).atan2(x as f32); // in radians
+//direction is set to point to qibala direction at home 
+        let dir = if theta < -7. * PI / 8. {
+            Direction::West
+        } else if theta < -5. * PI / 8. {
+            Direction::Southwest
+        } else if theta < -3. * PI / 8. {
+            Direction::South
+        } else if theta < -PI / 8. {
+            Direction::Southeast
+        } else if theta < PI / 8. {
+            Direction::East
+        } else if theta < 3. * PI / 8. {
+            Direction::Northeast
+        } else if theta < 5. * PI / 8. {
+            Direction::North
+        } else if theta < 7. * PI / 8. {
+            Direction::Northwest
+        } else {
+            Direction::North
         };
+
+        iprintln!(&mut itm.stim[0],"{:?}",lsm303dlhc.mag().unwrap());
 
         leds.iter_mut().for_each(|led| led.off());
         leds[dir].on();
 
-        delay.delay_ms(1_000_u16);
+        delay.delay_ms(1000_u16);
     }
 }
